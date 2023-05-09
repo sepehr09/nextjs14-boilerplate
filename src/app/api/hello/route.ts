73d132fff0029079581ef14886bc8d85c4f1bea2
sepehr09 <sepehr09@gmail.com>
2023-05-09 10:20:34 +0330

    feat: update tailwindconfig + next13-4-sample (#6)
    Co-authored-by: sepehr karimi <karimi.sepehr2@gmail.com>

--- src/app/api/hello/route.ts
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line import/prefer-default-export, @typescript-eslint/no-unused-vars
-export async function GET(request: Request) {
-  return new Response("Hello, Next.js!");
-}
