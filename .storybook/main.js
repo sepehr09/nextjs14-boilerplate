947d650e4a7a77cbb5cde503c0c5877005c67cf6
sepehr karimi <karimi.sepehr2@gmail.com>
2023-12-24 09:56:04 +0330

    feat: storybook+svgr

---
+++ .storybook/main.js
@@ -0,0 +1,37 @@
+/** @type { import('@storybook/nextjs').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions"
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {}
+  },
+  docs: {
+    autodocs: "tag"
+  },
+  staticDirs: [path.resolve(__dirname, "../public")],
+  webpackFinal: async (config, { configType }) => {
+    // path alias
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "../src/")
+    };
+
+    // svgr
+    const fileLoaderRule = config.module.rules.find(rule => rule.test && rule.test.test(".svg"));
+    fileLoaderRule.exclude = /\.svg$/;
+    config.module.rules.push({
+      test: /\.svg$/,
+      enforce: "pre",
+      loader: require.resolve("@svgr/webpack")
+    });
+
+    return config;
+  }
+};
+export default config;
