476d8d1d7c0596be8b4da41c70f407d339ccb855
sepehr09 <sepehr09@gmail.com>
2024-04-06 15:35:00 +0330

    feat: upgrade-to-14 (#12)
    Co-authored-by: sepehr karimi <karimi.sepehr2@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.scss";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
