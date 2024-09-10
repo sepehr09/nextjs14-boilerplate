999c8a7465afce72e8c7c86ab6426fc09fd5883a
sepehr09 <sepehr09@gmail.com>
2024-09-10 12:36:20 +0330

    Feat/update packages (#15)
    * feat: update-packages
    
    ---------
    
    Co-authored-by: sepehr karimi <karimi.sepehr2@gmail.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,19 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)"
+      }
+    }
   },
   plugins: []
-} satisfies Config;
+};
+export default config;
