f2f208f631b7f0e0dfd188d0e028f56dbe10f4ac
sepehr karimi <karimi.sepehr2@gmail.com>
2024-09-10 12:28:10 +0330

    feat: update-packages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -10,4 +10,5 @@ export default {
     extend: {}
   },
   plugins: []
-} satisfies Config;
+};
+export default config;
