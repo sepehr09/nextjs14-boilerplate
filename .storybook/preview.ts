be6496411a33101108776dd0a79e29512f694c4e
sepehr karimi <karimi.sepehr2@gmail.com>
2024-10-14 14:28:50 +0330

    feat: update-storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,7 @@
-/** @type { import('@storybook/react').Preview } */
+import type { Preview } from "@storybook/react";
 import "../src/styles/globals.css";
 
-const preview = {
+const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
